459dd58b17586a45bcf1c486a8fa168bdec744ac
David F Mason <david_mason@unc.edu>
2016-03-10 14:14:25 -0500

    reading attachments

--- src/main/java/phish/MailChecker.java
+++ src/main/java/phish/MailChecker.java
@@ -1,8 +1,5 @@
 package phish;
 
-import java.util.Iterator;
-import java.util.List;
-
 import microsoft.exchange.webservices.data.autodiscover.IAutodiscoverRedirectionUrl;
 import microsoft.exchange.webservices.data.core.ExchangeService;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
@@ -12,8 +9,7 @@ import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
 import microsoft.exchange.webservices.data.property.complex.Attachment;
 import microsoft.exchange.webservices.data.property.complex.AttachmentCollection;
-import microsoft.exchange.webservices.data.property.complex.FileAttachment;
-import microsoft.exchange.webservices.data.property.complex.ItemId;
+import microsoft.exchange.webservices.data.property.complex.ItemAttachment;
 import microsoft.exchange.webservices.data.search.FindItemsResults;
 import microsoft.exchange.webservices.data.search.ItemView;
 
@@ -26,31 +22,25 @@ public class MailChecker {
     service.setCredentials(credentials);
     try {
       service.autodiscoverUrl("newphish@unc.edu", new RedirectionUrlCallback());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    ItemView view = new ItemView(1);
-    FindItemsResults<Item> findResults;
-
-    try {
+      ItemView view = new ItemView(100);
+      FindItemsResults<Item> findResults;
       findResults = service.findItems(WellKnownFolderName.Inbox, view);
 
       for (Item item : findResults.getItems()) {
-        System.out.println("ID:  " + item.getId());
-        System.out.println("From:  " + item.getLastModifiedName());
-        System.out.println("Subject:  " + item.getSubject());
         System.out.println("Attachment:  " + item.getHasAttachments());
         item.load();
         System.out.println("Body:  " + item.getBody());
         AttachmentCollection attachments = item.getAttachments();
-        System.out.println(attachments.getCount());
+        System.out.println(attachments.getCount() + "attachments.");
 
         for (Attachment attachment : attachments) {
-          System.out.println("hello");
           System.out.println("Attachment name:  " + attachment.getName());
           System.out.println(attachment.getContentType());
           if ("message/rfc822".equals(attachment.getContentType())) {
             System.out.println("The attachment is a message.");
+            ItemAttachment itemAttachment = (ItemAttachment) attachment;
+            itemAttachment.load();
+            System.out.println(itemAttachment.getItem().getBody());
           }
         }
       }
@@ -60,9 +50,7 @@ public class MailChecker {
     service.close();
   }
 
-  /*
-   * (non-Javadoc)
-   * 
+  /**
    * I found this under "Responding to Autodiscover Redirecting" on this page:
    * https://github.com/OfficeDev/ews-java-api/wiki/Getting-Started-Guide
    */
